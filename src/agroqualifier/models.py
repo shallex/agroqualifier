287061bb5bc005ee2937d7dbe5d58ac7d80cb6aa
Ivanborisov80 <136759598+Ivanborisov80@users.noreply.github.com>
2024-05-24 22:38:12 +0300

    Update models.py

--- src/agroqualifier/models.py
+++ src/agroqualifier/models.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class VGG16_NET(nn.Module):
+class VGG16(nn.Module):
     def __init__(self):
-        super(VGG16_NET, self).__init__()
+        super(VGG16, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
 
