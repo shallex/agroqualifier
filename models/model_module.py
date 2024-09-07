1894f991ff63ab2d44e099c2effe80bf016d67ae
Alexander Sharshavin <a.sharshavin@tinkoff.ru>
2024-09-07 15:32:33 +0300

    add bbox drawing

--- models/model_module.py
+++ models/model_module.py
@@ -33,6 +33,31 @@ class MandarinSegmentationModel(L.LightningModule):
             masks = masks.squeeze(1)
         output_image = draw_segmentation_masks(image, masks, alpha=0.5, colors="blue")
         return output_image
+    
+    def log_images(self, images, preds, targets):
+        images_to_show = []
+        caption = []
+        for i in range(min(5, len(images))):
+            image = images[i].cpu()
+            for t, m, s in zip(image, MEAN, STD):
+                t.mul_(s).add_(m)
+            image = torch.clip(image * 255, 0, 255).to(torch.uint8)
+
+            # Predicted image
+            pred_boxes = preds[i]["boxes"].long()
+            pred_labels = [f"Score: {score:.3f}" for score in preds[i]["scores"]]
+            output_image = draw_bounding_boxes(image, pred_boxes, pred_labels, colors="red")
+            output_image = self.draw_mask(output_image, preds[i])
+            images_to_show.append(output_image)
+            num_preds = preds[i]["labels"].shape[0]
+            caption.append(f"Predicted, num predicted objects {num_preds}")
+
+            # Ground Truth
+            output_image = self.draw_mask(image, targets[i])
+            images_to_show.append(output_image)
+            caption.append("Ground Truth")
+
+        self.logger.log_image(key=f"validation batch, thr={self.threshold}", images=images_to_show, caption=caption)
 
     @torch.inference_mode()
     def validation_step(self, batch, batch_idx):
@@ -41,24 +66,7 @@ class MandarinSegmentationModel(L.LightningModule):
         preds = self.model(images, targets)
 
         if batch_idx == 0:
-            images_to_show = []
-            caption = []
-            for i in range(min(5, len(images))):
-                image = images[i].cpu()
-                for t, m, s in zip(image, MEAN, STD):
-                    t.mul_(s).add_(m)
-                image = torch.clip(image * 255, 0, 255).to(torch.uint8)
-
-                output_image = self.draw_mask(image, preds[i])
-                images_to_show.append(output_image)
-                num_preds = preds[i]["labels"].shape[0]
-                caption.append(f"Predicted, num predicted objects {num_preds}")
-
-                output_image = self.draw_mask(image, targets[i])
-                images_to_show.append(output_image)
-                caption.append("Ground Truth")
-
-            self.logger.log_image(key=f"validation batch, thr={self.threshold}", images=images_to_show, caption=caption)
+            self.log_images(images, preds, targets)
 
         preds = [{k: (v.squeeze(1).to(torch.bool) if k == "masks" else v) for k, v in p.items()} for p in preds]
         targets = [{k: (v.to(torch.bool) if k == "masks" else v) for k, v in p.items()} for p in targets]
